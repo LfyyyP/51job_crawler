8d1cf89c8a28b59c0ac63d780d815f2268a0124c
LfyyyP <tmzg2073@gmail.com>
2023-04-17 14:52:58 +0800

    测试用

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-import cities_list
+import calendar
+from datetime import datetime
+import pandas as pd
+from dateutil.relativedelta import relativedelta
 
-
-job_area = cities_list.cities_
+column = ['company_name',
+          'company_type',
+          'company_size',
+          'update_time',
+          'job_name',
+          'job_area',
+          'job_tags',
+          'salary',
+          'company_link'
+          ]
+df = pd.read_csv('data.csv', names=column)
+time_list = df['update_time']
+month_now = datetime.now().strftime('%Y-%m')
+print(month_now)
+month_last = datetime.now().date() - relativedelta(months=1)
+print(month_last)
