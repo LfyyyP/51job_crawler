ffc736f6190e584f8b62d3de4d101e28f1bc45b0
LfyyyP <tmzg2073@gmail.com>
2023-04-17 13:05:38 +0800

    对数据进行编辑

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import requests
 # import pprint
 import csv
+import cities_list
 
 
 # 爬取数据
@@ -22,7 +23,7 @@ def craw_data(url, headers, data):
     # 循环写入网页中的数据
     for index in response.json()['resultbody']['job']['items']:
         dit = {
-            'company_name': index['companyName'],
+            'company_name': index['fullCompanyName'],
             'company_type': index['companyTypeString'],
             'company_size': index['companySizeString'],
             'update_time': index['updateDateTime'],
@@ -32,7 +33,7 @@ def craw_data(url, headers, data):
             'salary': index['provideSalaryString'],
             'company_link': index['companyHref'],
         }
-        print(dit)
+        # print(dit)
         csv_writer.writerow(dit)
 
 
@@ -42,7 +43,7 @@ if __name__ == '__main__':
     # 浏览真实网页所需参数
     data = {
         'api_key': '51job',
-        'timestamp': '1681188460',
+        'timestamp': '1681701772',
         'keyword': 'plc',
         'searchType': '2',
         'function': '',
@@ -58,19 +59,20 @@ if __name__ == '__main__':
         'companyType': '',
         'companySize': '',
         'jobType': '',
+        # 发布日期筛选
         'issueDate': '',
         'sortType': '0',
         'pageNum': '1',
         'requestId': '',
         # 可以直接指定爬取数据个数，经测试，最大值为20000
-        'pageSize': '20000',
+        'pageSize': '10000',
         'source': '1',
         'accountId': '',
         'pageCode': 'sou|sou|soulb',
     }
     # 请求头
     headers = {
-        'sign': '593b36966e1fea654dd4576286192213fdfab0aac4c0bdbcabdd314cd672416c',
+        'sign': '21979efb8f05715fca87ba0a3eacf5fc9490d1f10c8714e6b8e899a17af30aee',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
     }
     craw_data(url, headers, data)
---
+++ test.py
@@ -0,0 +1,4 @@
+import cities_list
+
+
+job_area = cities_list.cities_
