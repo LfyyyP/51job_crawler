bb85485415c626050d3d3b165de22ef3fdddce45
LfyyyP <tmzg2073@gmail.com>
2023-04-23 13:19:01 +0800

    new branch

--- crawler.py
+++ crawler.py
@@ -1,14 +1,12 @@
 import requests
-# import pprint
 import csv
-import cities_list
 
 
 # 爬取数据
 def craw_data(url, headers, data):
     response = requests.get(url=url, headers=headers, params=data)
     # 创建csv文件
-    f = open('data.csv', mode='a', encoding='utf-8', newline='')
+    f = open('data_test.csv', mode='a', encoding='utf-8', newline='')
     csv_writer = csv.DictWriter(f, fieldnames=[
         'company_name',
         'company_type',
@@ -49,7 +47,7 @@ if __name__ == '__main__':
         'function': '',
         'industry': '',
         # 不同参数指定不同区域,这里指定了云、贵、川、渝
-        'jobArea': '060000,090000,250000,260000',
+        'jobArea': '',
         'jobArea2': '',
         'landmark': '',
         'metro': '',
@@ -73,6 +71,75 @@ if __name__ == '__main__':
     # 请求头
     headers = {
         'sign': '21979efb8f05715fca87ba0a3eacf5fc9490d1f10c8714e6b8e899a17af30aee',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/111.0.0.0 Safari/537.36',
     }
+    cities_ = {
+        '北京': '010000,',
+        '上海': '020000,',
+        '天津': '050000,',
+        '重庆': '060000,',
+        '广东': '030000,',
+        '江苏': '070000,',
+        '浙江': '080000,',
+        '四川': '090000,',
+        '海南': '100000,',
+        '福建': '110000,',
+        '山东': '120000,',
+        '江西': '130000,',
+        '广西': '140000,',
+        '安徽': '150000,',
+        '河北': '160000,',
+        '河南': '170000,',
+        '湖北': '180000,',
+        '湖南': '190000,',
+        '陕西': '200000,',
+        '山西': '210000,',
+        '黑龙江': '220000,',
+        '辽宁': '230000,',
+        '吉林': '240000,',
+        '云南': '250000,',
+        '贵州': '260000,',
+        '甘肃': '270000,',
+        '内蒙古': '280000,',
+        '宁夏': '290000,',
+        '西藏': '300000,',
+        '新疆': '310000,',
+        '青海': '320000,',
+    }
+
+    parameter = {
+        'api_key': '51job',
+        'timestamp': '1681701772',
+        'keyword': 'plc',
+        'searchType': '2',
+        'function': '',
+        'industry': '',
+        # 不同参数指定不同区域,这里指定了云、贵、川、渝
+        'jobArea': '060000,090000,250000,260000',
+        'jobArea2': '',
+        'landmark': '',
+        'metro': '',
+        'salary': '',
+        'workYear': '',
+        'degree': '',
+        'companyType': '',
+        'companySize': '',
+        'jobType': '',
+        # 发布日期筛选
+        'issueDate': '',
+        'sortType': '0',
+        'pageNum': '1',
+        'requestId': '',
+        # 可以直接指定爬取数据个数，经测试，最大值为20000
+        'pageSize': '10000',
+        'source': '1',
+        'accountId': '',
+        'pageCode': 'sou|sou|soulb',
+    }
+    for i in list(cities_.keys()):
+        print(i, end=' ')
+    area = input('\n请输入想要爬取的城市名称(务必从上表中选取！)：')
+    area = cities_[area]
+    data['jobArea'] = area
     craw_data(url, headers, data)
